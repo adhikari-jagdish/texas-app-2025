adbf52f4706d65da2ee73af69eafc3d43d645720
Jagadish <adhikari.jagdish5200@gmail.com>
2025-11-27 06:47:05 +0545

    Fragments implementation

---
+++ app/src/main/java/com/texas/texasmobileapp/FragmentActivity.java
@@ -0,0 +1,46 @@
+package com.texas.texasmobileapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class FragmentActivity extends AppCompatActivity {
+    Button btnChangeFragment;
+    FragmentOne fragmentOne = new FragmentOne();
+    FragmentTwo fragmentTwo = new FragmentTwo();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_fragment);
+        btnChangeFragment = findViewById(R.id.btn_change_fragment);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnChangeFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fr_activity_fragment, fragment);
+        fragmentTransaction.commit();
+    }
+}
