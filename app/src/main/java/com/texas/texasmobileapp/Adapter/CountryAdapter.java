b4b8425cd3a75691a770ce00c955743fe989d43f
Jagadish <adhikari.jagdish5200@gmail.com>
2025-12-17 07:26:34 +0545

    Recyclerview completed

--- app/src/main/java/com/texas/texasmobileapp/Adapter/CountryAdapter.java
+++ app/src/main/java/com/texas/texasmobileapp/Adapter/CountryAdapter.java
@@ -1,13 +1,47 @@
 package com.texas.texasmobileapp.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CountryAdapter {
+import com.texas.texasmobileapp.Model.CountryModel;
+import com.texas.texasmobileapp.R;
+
+import java.util.ArrayList;
+
+public class CountryAdapter extends RecyclerView.Adapter<ViewHolder> {
+    Context context;
+    ArrayList<CountryModel> countryList;
+
+    public CountryAdapter (Context activityContext, ArrayList<CountryModel> countryList){
+        this.context = activityContext;
+        this.countryList = countryList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.custom_country_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int index) {
+        final CountryModel countryModel = countryList.get(index);
+        holder.customCountryCardImage.setImageResource(countryModel.getIcon());
+        holder.customCountrycardTextView.setText(countryModel.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return countryList.size();
+    }
 }
 
 class ViewHolder extends RecyclerView.ViewHolder{
@@ -16,5 +50,8 @@ class ViewHolder extends RecyclerView.ViewHolder{
 
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        customCountryCardImage = itemView.findViewById(R.id.iv_custom_country_card);
+        customCountrycardTextView = itemView.findViewById(R.id.tv_custom_country_card);
     }
 }
--- app/src/main/java/com/texas/texasmobileapp/RecyclerViewActivity.java
+++ app/src/main/java/com/texas/texasmobileapp/RecyclerViewActivity.java
@@ -7,8 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.texas.texasmobileapp.Adapter.CountryAdapter;
+import com.texas.texasmobileapp.Model.CountryModel;
+
+import java.util.ArrayList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
+
+    RecyclerView countriesRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,21 @@ public class RecyclerViewActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        countriesRecyclerView = findViewById(R.id.recyclerview_countries);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ArrayList<CountryModel> countries = new ArrayList<>();
+        countries.add(new CountryModel("Nepal", R.mipmap.nepal_flag));
+        countries.add(new CountryModel("USA", R.mipmap.ic_launcher));
+        countries.add(new CountryModel("Japan", R.mipmap.ic_launcher_round));
+
+        CountryAdapter countryAdapter = new CountryAdapter(RecyclerViewActivity.this, countries);
+        countriesRecyclerView.setAdapter(countryAdapter);
+
     }
 }
