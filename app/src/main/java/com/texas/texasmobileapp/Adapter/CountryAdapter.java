606fba979dff67310b6f3d5fe1370188f1b0097c
Jagadish <adhikari.jagdish5200@gmail.com>
2025-12-17 07:02:01 +0545

    CountryAdapter Added

---
+++ app/src/main/java/com/texas/texasmobileapp/Adapter/CountryAdapter.java
@@ -0,0 +1,20 @@
+package com.texas.texasmobileapp.Adapter;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CountryAdapter {
+}
+
+class ViewHolder extends RecyclerView.ViewHolder{
+    ImageView customCountryCardImage;
+    TextView customCountrycardTextView;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
